eb2b21ebe87b76f8c4d0247458ae26d991e50039
Acrata <hxtr227@gmail.com>
2023-07-25 09:23:52 +0200

    Install routing with react router

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello Stiinta!</div>,
+  },
+  {
+    path: "/test",
+    element: <div>Hello Test!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
