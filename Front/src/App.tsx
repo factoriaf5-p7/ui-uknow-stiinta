ae271110413a8485d9eb143d83ada8a92434e50c
Acrata <hxtr227@gmail.com>
2023-07-29 01:34:24 +0200

    Typo in Home import

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Home from './pages/HeroImg';
+import Home from './pages/Home';
 import Protected from './pages/Protected';
 import { AuthProvider } from './context/AuthProvider';
 import RequireAuth from './components/RequireAuth';
@@ -13,8 +13,8 @@ const App: FC = () => {
       <AuthProvider>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/" element={<HeroImg />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<RequireAuth allowedRoles={['user']} />}>
