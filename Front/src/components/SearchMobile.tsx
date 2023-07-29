f296ffffb2ac00571bd526c161204407f23b31fc
Acrata <hxtr227@gmail.com>
2023-07-29 16:34:25 +0200

    Search bar y bottom navigation inicializado

---
+++ Front/src/components/SearchMobile.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input"
+
+export function SearchMobile() {
+  return(
+    <div className="bg-[url('/header-bg.svg')] bg-no-repeat bg-cover px-4 py-6 rounded-bl-3xl rounded-br-3xl">
+      <Input placeholder="Buscar" />
+    </div>
+  )
+}
