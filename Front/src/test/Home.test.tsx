6e99d271c3c3a04ca2472e859ec3a74b8880840c
Angela-GM <angelagm0112@gmail.com>
2023-07-30 19:21:59 +0200

    update

--- Front/src/test/Home.test.tsx
+++ Front/src/test/Home.test.tsx
@@ -1,30 +1,21 @@
 import { beforeEach, describe, test, screen, expect } from "vitest";
-import CardHome from "../components/CardHome";
-import { render } from "vitest/dom"; // Asegúrate de importar render desde vitest/dom
+import { render, screen, waitFor } from "@testing-library/react";import { MemoryRouter } from "react-router-dom";
+import  Home  from "../pages/Home";
 
 
 describe("CardHome", () => {
   beforeEach(async () => {
-    // Renderiza el componente
-    render(<CardHome />);
-    // Espera a que se carguen los cursos (Loading... se ha ido)
-    await screen.findByText("Loading...", { exact: false });
+    render(
+        <MemoryRouter initialEntries={["/"]}>
+            <Home />
+        </MemoryRouter>
+    )   
   });
 
-  test("should render CardHome page", async () => {
-    // Verifica que los cursos se rendericen correctamente
-    const courseNames = ["Curso", "Curso2"]; // Cambia esto con los nombres reales de los cursos
-    for (const courseName of courseNames) {
-      const elementsWithText = screen.getAllByText(courseName, { exact: false });
-      expect(elementsWithText).toHaveLength(1);
-      expect(elementsWithText[0]).toBeInTheDocument();
-    }
-
-    // Verifica que se rendericen los botones de "Comprar"
-    const buttonsComprar = screen.getAllByText("Comprar", { exact: false });
-    expect(buttonsComprar).toHaveLength(2); // Debería haber dos botones con el texto "Comprar"
-    for (const button of buttonsComprar) {
-      expect(button).toBeInTheDocument();
-    }
-  });
+  test("renders a link to the 'Comprar' page", async () => {
+    await waitFor(() => {
+        const comprarText = screen.getByAllText(/Comprar/i);
+        expect(comprarText).toBeInTheDocument();
+    })
+  })
 });
