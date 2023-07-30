612e95cf8c21961965cae654789ce5543888806c
Angela-GM <angelagm0112@gmail.com>
2023-07-30 18:31:38 +0200

    pruebas test

--- Front/src/test/Home.test.tsx
+++ Front/src/test/Home.test.tsx
@@ -1,21 +1,30 @@
-import { describe, test, expect, beforeEach } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import Home  from "../pages/Home";
+import { beforeEach, describe, test, screen, expect } from "vitest";
+import CardHome from "../components/CardHome";
+import { render } from "vitest/dom"; // Asegúrate de importar render desde vitest/dom
 
-describe("Home component", () => {
-    beforeEach(() => {
-        render(
-            <MemoryRouter initialEntries={["/"]}>
-            <Home />
-          </MemoryRouter>
-        );
-    });
 
-    test("renders a link to the 'Comprar' page", async () => {
-        await waitFor(() => {
-            const comprarText = screen.getByText(/Comprar/i);
-            expect(comprarText).toBeInTheDocument();//)
-        })
-    })
+describe("CardHome", () => {
+  beforeEach(async () => {
+    // Renderiza el componente
+    render(<CardHome />);
+    // Espera a que se carguen los cursos (Loading... se ha ido)
+    await screen.findByText("Loading...", { exact: false });
+  });
+
+  test("should render CardHome page", async () => {
+    // Verifica que los cursos se rendericen correctamente
+    const courseNames = ["Curso", "Curso2"]; // Cambia esto con los nombres reales de los cursos
+    for (const courseName of courseNames) {
+      const elementsWithText = screen.getAllByText(courseName, { exact: false });
+      expect(elementsWithText).toHaveLength(1);
+      expect(elementsWithText[0]).toBeInTheDocument();
+    }
+
+    // Verifica que se rendericen los botones de "Comprar"
+    const buttonsComprar = screen.getAllByText("Comprar", { exact: false });
+    expect(buttonsComprar).toHaveLength(2); // Debería haber dos botones con el texto "Comprar"
+    for (const button of buttonsComprar) {
+      expect(button).toBeInTheDocument();
+    }
+  });
 });
