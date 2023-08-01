b5dc9454fa1c6b58f6f6cd039b41ef46d14289ed
Arielparicio28 <arielaparicio100@gmail.com>
2023-08-01 18:29:57 +0200

    Forms estilos terminados

--- Front/src/pages/HeroImg.tsx
+++ Front/src/pages/HeroImg.tsx
@@ -31,11 +31,9 @@ function HeroImg({ showHeroImage, setShowHeroImage }: HeroImgProps) {
   }, [showDelayedButton]);
 
   return (
-    <div className="flex flex-col lg:flex-row items-center bg-gray-100 h-screen">
+    <div className="flex flex-col lg:flex-row items-center bg-background h-screen">
       <div className="p-6 lg:self-start">
-        <h1 className="text-xl font-medium text-right text-sky-900 italic hover:not-italic">
-          Uknow
-        </h1>
+        <img  className='m-auto' src="logo.svg" alt="logo" />
       </div>
       {showHeroImage && (
         <motion.div
@@ -63,19 +61,19 @@ function HeroImg({ showHeroImage, setShowHeroImage }: HeroImgProps) {
         >
           <h1 className="text-3xl mt-5 font-medium text-center lg:text-right">
             Choose your <br />
-            <span className="text-orange-500 italic hover:not-italic ">
+            <span className="text-orange-500 ">
               Learning{" "}
             </span>
             path
           </h1>
-          <h6 className="mt-4 text-center lg:text-right ">
+          <h6 className="mt-4 text-center lg:text-right text-hero ">
             Explore all the most exciting job roles based <br /> on your
             interest and study major.
           </h6>
           <div className="lg:flex lg:justify-end mt-4 flex justify-end ">
-            <button onClick={handleButtonClick}>
-              <img className="py-6 animate-pulse " src="button.png" alt="" />
-            </button>
+            <button onClick={handleButtonClick} className="  animate-pulse bg-btnOscuro hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Continue →
+             </button>
           </div>
         </motion.div>
       )}
--- Front/src/pages/Register.tsx
+++ Front/src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Button from "@/components/ui/Button";
 
 const Register: React.FC = () => {
   const [name, setName] = useState("");
@@ -66,88 +67,107 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen p-6  flex flex-col md:items-center md:justify-center">
-      <h2 className="text-3xl font-bold mb-2 text-center">Create an account</h2>
-      <p className="text-center m-3 italic hover:not-italic">
+    <div className="bg-background min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+       <img className='m-auto' src="logo.svg" alt="logo:Uknow" />
+       <div>
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-dark">Create an account</h2>
+      <p className="mt-2 text-center text-sm text-text">
         Uknow is a cutting-edge application designed for programming enthusiasts
         and aspiring developers. <br /> Uknow has you covered.
       </p>
       {errorMessage && (
         <p className="text-red-500 text-center my-4">{errorMessage}
-        <img src="advertencia.png" className="block mx-auto mt-2 max-w-24 max-h-24" alt="" />
+        <img src="advertencia.png" className="block mx-auto mt-2 max-w-24 max-h-24" alt="Imagen de advertencia" />
         </p>
       )}
-      <div>
-              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+       </div>
+       <form className="mt-8 space-y-6" onSubmit={(e) => e.preventDefault()}>
+      <div className="rounded-md shadow-sm space-y-4" > 
+              <div>
+              <label htmlFor="name" className="block text-sm font-medium text-dark">
                 <b>Name</b>
               </label>
               <input 
                 id="name"
                 name="name"
                 type="name"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-dark rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
-             <div>
-              <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+
+             <div> 
+              <label htmlFor="lastName" className="block text-sm font-medium text-dark">
                 <b>Last Name</b>
               </label>
               <input
                 id="lastName"
                 name="lastName"
                 type="text"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-dark rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
-            </div> 
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              </div>
+             <div> 
+              <label htmlFor="email" className="block text-sm font-medium text-dark">
                 <b>Email</b>
               </label>
               <input
                 id="email"
                 name="email"
                 type="email"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-dark rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-            </div>
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+          </div>
+          <div> 
+              <label htmlFor="password" className="block text-sm font-medium text-dark">
                 <b>Password</b>
               </label>
               <input
                 id="password"
                 name="password"
                 type="password"
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-dark rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+             </div>
+              </div>
+              <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember_me"
+                name="remember_me"
+                type="checkbox"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+              />
+              <label htmlFor="remember_me" className="ml-2 block text-sm text-remember">
+                Remember me
+              </label>
             </div>
-      <p className="text-center p-6 ">
-        <a href="" className="text-blue-500 underline">
-          Forgot Password?
-        </a>
-      </p>
-      <div className="mb-4 flex items-center ">
-        <input type="checkbox" className="mr-2" />
-        <label>Remember Me</label>
+
+            <div className="text-sm">
+              <a href="#" className="font-medium text-dark hover:text-text">
+                ¿Forgot your password?
+              </a>
+            </div>
+          </div>
+          <div className='flex w-full '>
+         <Button color="bg-btnOscuro" text="text-white" >
+        Sign Up
+      </Button>
       </div>
-      <div className="mb-4">
-        <button
-          className="w-full bg-blue-900 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          onClick={handleRegister}
-        >
-          Sign up
-        </button>
-        <p className="p-6">You don't have an account yet? <span className="text-orange-500 underline p-1">Sign in</span></p>
+   
+        <p className="p-6 text-text">You don't have an account yet? <span className="text-orange-500 underline p-1">Sign in</span></p>
+     
+      </form>
       </div>
-    </div> 
+      </div>
   );
 };
 
