5be3a936b3f672288510aee21fff8b58cda51893
Angela-GM <angelagm0112@gmail.com>
2023-08-01 09:33:18 +0200

    quitar contenido de la card

--- Front/src/components/CardHome.tsx
+++ Front/src/components/CardHome.tsx
@@ -12,18 +12,7 @@ function CardHome() {
   const [courses, setCourses] = useState<Course[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-   // Obtener las 15 primeras palabras de la descripción
-   const truncateDescription = (str:string, numWords: number) => {
-    const words = str.split(' ');
-    const truncated = words.slice(0, numWords).join(' ');
-
-    if (words.length > numWords) {
-      return `${truncated}...`;
-    }
-
-    return truncated;
-  };
-
+   
   useEffect(() => {
     // Definir una función asincrónica dentro del useEffect para poder usar 'await'
     const fetchCourses = async () => {
@@ -60,9 +49,9 @@ function CardHome() {
               </div>
               <div className="contain-section py-4 px-6 bg-white rounded-xl">
                 <h3 className="text-title font-bold mb-3">{course.name}</h3>
-                <p className="text-text mb-3">
-                  {truncateDescription(course.content, 15)}
-                </p>
+                {/* <p className="text-text mb-3">
+                  {truncateDescription(content, 15)}
+                </p> */}
                 <div className="tags flex gap-2 mb-4 flex-wrap">
                   {course.tags.map((tag, index) => (
                     <h3 key={index} className="tag bg-gray-100 rounded-md px-1.5 py-1 text-text text-sm">
--- Front/src/components/Modal.tsx
+++ Front/src/components/Modal.tsx
@@ -13,20 +13,30 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 type ModalProps = Partial<Course> & { textButton: string };
 
 function Modal({ textButton, name, content }: ModalProps): JSX.Element {
+  // Obtener las 15 primeras palabras de la descripción
+  const truncateDescription = (str: string, numWords: number) => {
+    const words: string[] = str.split(" ");
+    const truncated: string = words.slice(0, numWords).join(" ");
+
+    if (words.length > numWords) {
+      return `${truncated}...`;
+    }
+
+    return truncated;
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
         <Button color="bg-btnClaro" text="text-text" children={textButton} />
       </DialogTrigger>
       <DialogContent>
-        <div className="image-section bg-[url('/public/img-course.svg')] bg-no-repeat h-36 bg-cover bg-center  rounded-t-xl relative">
-
-        </div>
+        <div className="image-section bg-[url('/public/img-course.svg')] bg-no-repeat h-36 bg-cover bg-center  rounded-t-xl relative"></div>
         <DialogHeader>
           <DialogTitle>{name}</DialogTitle>
           <DialogDescription>
             <ScrollArea className="h-[200px] w-[auto] rounded-md border p-4">
-              {content}
+              {truncateDescription(content, 15)}
             </ScrollArea>
           </DialogDescription>
           <Button color="bg-btnOscuro" text="text-white" children="Comprar" />
