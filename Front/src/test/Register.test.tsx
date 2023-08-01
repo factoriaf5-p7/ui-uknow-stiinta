1b25d2215aa3b5ace91b2bdc92c7e04bbd081e8d
carlos-saiz <useles123456789@gmail.com>
2023-08-01 17:25:07 +0200

    He añadido mas test unitarios a register y falta arreglar test con mock

--- Front/src/test/Register.test.tsx
+++ Front/src/test/Register.test.tsx
@@ -19,4 +19,32 @@ describe("Register", () => {
         expect(screen.getByLabelText("Password")).toBeInTheDocument();
         expect(screen.getByRole('button', {name: /Sign up/i}))
       });
-});
+
+    test('display All fields are required.”', async () => {
+    expect(screen.queryByText(/Por favor rellene todos los campos/i)).not.toBeInTheDocument()
+    });    
+
+    test('display Password must be at least 8 characters.', async () => {
+    expect(screen.queryByText(/La contraseña debe tener al menos 8 caracteres./i)).not.toBeInTheDocument();
+    });
+
+    test("display 'Email is not valid.' when an invalid email is entered", async () => {
+    expect(screen.queryByText(/Email is not valid./i)).not.toBeInTheDocument();
+    });
+
+
+    const nameInput = screen.getByLabelText("Name");
+    const lastNameInput = screen.getByLabelText("Last Name");
+    const emailInput = screen.getByLabelText("Email");
+    const passwordInput = screen.getByLabelText("Password");
+    fireEvent.change(nameInput, { target: { value: 'Carlos' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Saiz' } });
+    fireEvent.change(emailInput, { target: { value: 'carlosgmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'asdzxcqwe' } });
+
+    fireEvent.click(screen.getByRole('button', {name: /Sign up/i}));
+    
+    test("display 'Email is not valid.' when an invalid email is entered", async () => {
+    expect(screen.queryByText(/Email is not valid./i)).toBeInTheDocument();
+    });
+}); 
