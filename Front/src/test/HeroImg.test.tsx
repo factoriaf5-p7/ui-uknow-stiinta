ea4acd837f9701c1a2e35bc48df6d4d31b851b40
carlos-saiz <useles123456789@gmail.com>
2023-08-02 13:03:52 +0200

    He añadido dos test de heroimg

---
+++ Front/src/test/HeroImg.test.tsx
@@ -0,0 +1,27 @@
+import { beforeEach, describe, test, expect } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import  HeroImg  from "../pages/HeroImg";
+
+describe("HeroImg", () => {
+    beforeEach(async () => {
+      render(
+        <MemoryRouter initialEntries={["/"]}>
+          <HeroImg showHeroImage={true} setShowHeroImage={() => {}} />
+        </MemoryRouter>
+      );
+    });
+  
+    test("renders the logo", () => {
+        const logo = screen.getByAltText("logo");
+        expect(logo).toBeInTheDocument();
+        expect(logo.tagName).toBe("IMG");
+      });
+
+     test("displays the hero image", () => {
+        const heroImage = screen.getByAltText("");
+        expect(heroImage).toBeInTheDocument();
+        expect(heroImage.tagName).toBe("IMG");
+      });
+
+});
